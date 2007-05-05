c4b53d3f2e235d1b94234a40f45876a2fd51d620
root <root@Morgoth.localdomain>
2007-05-05 20:32:19 +0200

    Some indentation fixes
    Signed-off-by: root <root@Morgoth.localdomain>

--- lib/conexionbd.php
+++ lib/conexionbd.php
@@ -1,37 +1,37 @@
 <?
-	include 'configuracion.php';
+include 'configuracion.php';
 
-	function f_conectar(){
-		if (!($link=mysql_connect($db_host,$username,$password)))
-		{
-			echo('<script>alert("Error conectando a la base de datos");</script>');
-			exit();
-		}
-
-		if (!mysql_select_db($database,$link))
-		{
-			echo "Error seleccionando la base de datos.";
-			exit();
-		}
-		return $link;
+function f_conectar(){
+	if (!($link=mysql_connect($db_host,$username,$password)))
+	{
+		echo "Error conectando a la base de datos";
+		exit();
 	}
 
-	function f_leer($lsql){
-		$link = f_conectar();
-		$lreg = mysql_query($lsql,$link);
-		return $lreg;
+	if (!mysql_select_db($database,$link))
+	{
+		echo "Error seleccionando la base de datos.";
+		exit();
 	}
+	return $link;
+}
 
-	function f_ejecutar($lsql){
-		$link = f_conectar();
-		mysql_db_query($database,$lsql);
-	}
+function f_leer($lsql){
+	$link = f_conectar();
+	$lreg = mysql_query($lsql,$link);
+	return $lreg;
+}
 
-	function f_desconectar($link){
-		mysql_close($link);
-	}
+function f_ejecutar($lsql){
+	$link = f_conectar();
+	mysql_db_query($database,$lsql);
+}
 
-	function f_error(){
-		echo mysql_errno().": ".mysql_error()."<BR>";
-	}
+function f_desconectar($link){
+	mysql_close($link);
+}
+
+function f_error(){
+	echo mysql_errno().": ".mysql_error()."<BR>";
+}
 ?>
