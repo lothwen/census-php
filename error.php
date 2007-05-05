4ef5b6c0a9addea5b2f7ea9d8b3390ff712e1b03
root <root@Morgoth.localdomain>
2007-05-05 21:49:12 +0200

    very simple error page. I'll be nice to add some image
    Signed-off-by: root <root@Morgoth.localdomain>

---
+++ error.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<meta http-equiv="Content-Style-Type" content="text/css">
+	<title>Euskai Census v1.0</title>
+	<link rel="STYLESHEET" href="style/estilo.css" type="text/css">
+	<link rel="STYLESHEET" href="style/estilo-tabla.css" type="text/css">
+</head>
+
+<body>
+<table class="container" width="70%" align="center">
+
+<tr>
+	<td class="header" colspan="4"><a href="index.php"><img border="0" alt="logo.png" src="images/logo.png"></a></td>
+</tr>
+
+<tr>
+	<td class="content" colspan="4">
+	<h1>ERROR DE ACCESO</h1>
+
+</td>
+</tr>
+
+<tr>
+	<td class="footer" colspan="4"><b>Euskai Eskaut Taldea  Census v1.0BETA Licensed under the GNU GPL2 License</b><br>
+	    <a href="http://validator.w3.org/check?uri=referer"><img
+	    class="imagen" src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>	
+	</td>
+</tr>
+
+</table>
+
+</body>
+</html>
