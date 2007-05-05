9f17eb3f547e9274b3c425ffddf827f1553bafd0
root <root@Morgoth.localdomain>
2007-05-05 22:45:33 +0200

    Try to fix an error in session_start(). Maybe had a whitespace before the function call ...
    Signed-off-by: root <root@Morgoth.localdomain>

--- lib/cabecera.php
+++ lib/cabecera.php
@@ -1,10 +1,9 @@
-<?php 
-	//Validar que el usuario tiene
- 	session_start();	
-	//Validar el usaurio 
-	if (!isset($val_usuario)) {		
-		header("Location: error.php");				
-	}
+<?php
+session_start();	
+//Validar si se ha logueado
+if (!isset($val_usuario)) {		
+	header("Location: error.php");				
+}
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
