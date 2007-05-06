b53f494ace541834ecb4da810bc01cf2d2b9687a
root <root@Morgoth.localdomain>
2007-05-06 21:54:39 +0200

    Styles error in error.php
    Signed-off-by: root <root@Morgoth.localdomain>

--- error.php
+++ error.php
@@ -11,21 +11,21 @@
 </head>
 
 <body>
-<table class="container" width="70%" align="center">
+<table id="container" width="70%" align="center">
 
 <tr>
-	<td class="header" colspan="4"><a href="index.php"><img border="0" alt="logo.png" src="images/logo.png"></a></td>
+	<td id="header" colspan="4"><a href="index.php"><img border="0" alt="logo.png" src="images/logo.png"></a></td>
 </tr>
 
 <tr>
-	<td class="content" colspan="4">
+	<td id="content" colspan="4">
 	<h1>ERROR DE ACCESO</h1>
 
 </td>
 </tr>
 
 <tr>
-	<td class="footer" colspan="4"><b>Euskai Eskaut Taldea  Census v1.0BETA Licensed under the GNU GPL2 License</b><br>
+	<td id="footer" colspan="4"><b>Euskai Eskaut Taldea  Census v1.0BETA Licensed under the GNU GPL2 License</b><br>
 	    <a href="http://validator.w3.org/check?uri=referer"><img
 	    class="imagen" src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>	
 	</td>
