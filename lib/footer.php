4c9abcd78aa053b7e5d0ba507c3977513e081e5d
root <root@wintermute.(none)>
2007-06-03 00:16:51 +0200

    Remove hardcoded version number. Added version.php file to change version number easily
    Signed-off-by: root <root@wintermute.(none)>

--- lib/footer.php
+++ lib/footer.php
@@ -1,8 +1,11 @@
+<?
+include 'version.php';
+?>
 </td>
 </tr>
 
 <tr>
-	<td id="footer" colspan="4"><b>Euskai Eskaut Taldea  Census v1.0BETA Licensed under the GNU GPL2 License</b><br>
+<td id="footer" colspan="4"><b>Euskai Eskaut Taldea  Census <?echo $version?> Licensed under the GNU GPL2 License</b><br>
 	    <a href="http://validator.w3.org/check?uri=referer"><img
 	    class="imagen" src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>	
 	</td>
