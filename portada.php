ec34b63bbb25ccf37339216af0a19f7b843bcc25
root <root@lothwen-desktop.(none)>
2007-06-05 15:25:47 +0200

    Changed introduction text
    Signed-off-by: root <root@lothwen-desktop.(none)>

--- portada.php
+++ portada.php
@@ -9,7 +9,18 @@ include 'lib/cab1.php';
 ?>
 	<center><h2> Census <?echo $version?></h2></center>
 
-<p>Esta es una versión en desarrollo de lo que será un herramienta muy útil para Euskai Eskaut Taldea.</p>
+<p> Bienvenid@ a Census</p>
+
+<p>Census es una simple aplicación web para llevar el censo de los chavales apuntados a un grupo eskaut. El objetivo
+de la misma es facilitarle la vida al coordinador del grupo, ya que de esta manera, los datos de los chavales se
+encuentran siempre accesibles y centralizados. Con esto evitamos el tener copias de censos desactualizados y permitimos el acceso a los datos de los chavales al resto de monitores.</p>
+<p>Entre las posibilidades que nos ofrece esta herramienta encontramos:</p>
+<ul>
+	<li> Añadir, modificar y eliminar chavales al censo.
+	<li> Hacer busquedas con varios criterios de busqueda.
+	<li> Exportar en formato pdf las busquedas que hagamos.
+	<li> Exportar etiquetas para pegar en sobres con la dirección de los chavales.
+</ul>
 
 <p> Si quieres visitar la pagina web del grupo, deberas dirigirte a <a href="http://euskai.org">http://euskai.org</a></p>
 
