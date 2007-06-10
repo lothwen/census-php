f90466fb6e405fe7c34e0757e3c43bf422e80c98
root <root@wintermute.(none)>
2007-06-10 15:17:05 +0200

    Add a total number of child per rama and a button to export all census
    Signed-off-by: root <root@wintermute.(none)>

--- exportar.php
+++ exportar.php
@@ -45,6 +45,50 @@ include 'lib/cab1.php';
 	</tr>
 </table>
 </form>
+
+<?
+$sSql = "SELECT COUNT(ID) FROM census GROUP BY RAMA";
+$result = f_leer($sSql);
+while($row = mysql_fetch_array($result))
+        $total_ramas[] = $row[0];
+
+?>
+
+<table align="center" border="0">
+        <tr>
+                <th>Rama</td>
+                <th>Total</td>
+        </tr>   
+                <td>Arduradunak</td>
+                <td><?echo $total_ramas[0]?></td>
+        </tr>   
+        <tr>
+                <td>Koskorrak</td>
+                <td><?echo $total_ramas[1]?></td>
+        </tr>   
+        <tr>
+                <td>Kaskondoak</td>
+                <td><?echo $total_ramas[2]?></td>
+        </tr>   
+        <tr>
+                <td>Oinarinak</td>
+                <td><?echo $total_ramas[3]?></td>
+        </tr>   
+        <tr>
+                <td>Azkarrak</td>
+                <td><?echo $total_ramas[4]?></td>
+        </tr>   
+        <tr>
+                <td>Trebeak</td>
+                <td><?echo $total_ramas[5]?></td>
+        </tr>   
+</table>
+
+<form action="pdf.php" method="post">
+        <input type="hidden" name="sentencia" value="SELECT * FROM censo">
+        <center><input type="submit" name="generar" value="Exportar Censo en formato pdf"></center>
+</form>
+
 <?
 include 'lib/footer.php';
 ?>
