06377784b71c882fbb53f90648ef4daebfe2938d
root <root@wintermute.(none)>
2007-06-10 22:32:13 +0200

    Removed all table design and make it to work with css layers
    Signed-off-by: root <root@wintermute.(none)>

--- lib/cab1.php
+++ lib/cab1.php
@@ -1,16 +1,16 @@
 <body>
-<table id="container" width="70%" align="center">
+<div id="container">
 
-<tr>
-	<td id="header" colspan="4"><a href="index.php"><img border="0" alt="logo.png" src="images/cabecera.gif"></a></td>
-</tr>
+	<div id="header">
+		<a href="index.php"><img border="0" alt="logo.png" src="images/cabecera.gif"></a>
+	</div>
 
-<tr>
 
-	<td class="menu"><a href="./formulario.php">Insertar un nuevo chaval/a</a></td>
-	<td class="menu"><a href="./busqueda.php">Realizar una busqueda</a></td>
-	<td class="menu"><a href="./exportar.php">Exportar datos</a></td>
-</tr>
-
-<tr>
-	<td id="content" colspan="4">
+	<div id="navigation">
+	<ul>
+		<li><a href="./formulario.php">Insertar un nuevo chaval/a</a></li>
+		<li><a href="./busqueda.php">Realizar una busqueda</a></li>
+		<li><a href="./exportar.php">Exportar datos</a></li>
+	</ul>
+	</div>
+	<div id="content">
--- lib/footer.php
+++ lib/footer.php
@@ -1,17 +1,14 @@
 <?
 include 'version.php';
 ?>
-</td>
-</tr>
 
-<tr>
-<td id="footer" colspan="4"><b>Euskai Eskaut Taldea  Census <?echo $version?> Licensed under the GNU GPL2 License</b><br>
+</div>
+<div id="footer"><b>Euskai Eskaut Taldea  Census <?echo $version?> Licensed under the GNU GPL2 License</b><br>
 	    <a href="http://validator.w3.org/check?uri=referer"><img
 	    class="imagen" src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>	
-	</td>
-</tr>
+</div>
 
-</table>
+</div>
 
 </body>
 </html>
