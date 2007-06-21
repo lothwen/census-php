c82fec147c7215be882bf08ab996c40d70a79cfe
root <root@Morgoth.localdomain>
2007-06-21 21:49:48 +0200

    Improve multi-theme support
    Signed-off-by: root <root@Morgoth.localdomain>

--- lib/cab1.php
+++ lib/cab1.php
@@ -5,14 +5,14 @@
 		<a href="index.php"><img border="0" alt="Cabacera" src="<?echo $THEMEDIR?>/img/cabecera.gif"></a>
 	</div>
 
-
-	<div id="navigation">
-	<ul>
-		<li><a href="./formulario.php">Insertar un nuevo chaval/a</a></li>
-		<li><a href="./busqueda.php">Realizar una busqueda</a></li>
-		<li><a href="./exportar.php">Exportar datos</a></li>
-		<li><a href="./actividadesl.php">Actividades</a></li>
-		<li><a href="./logout.php">Logout</a></li>
-	</ul>
-	</div>
 	<div id="content">
+		<div id="navigation">
+			<ul>
+				<li><a href="./formulario.php">Insertar un nuevo chaval/a</a></li>
+				<li><a href="./busqueda.php">Realizar una busqueda</a></li>
+				<li><a href="./exportar.php">Exportar datos</a></li>
+				<li><a href="./actividadesl.php">Actividades</a></li>
+				<li><a href="./logout.php">Logout</a></li>
+			</ul>
+		</div>
+		<div id="body">
