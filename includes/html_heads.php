a48529059a996797b60422ea3b0b4205724ecd25
Daniel Sánchez Ausin <danillo@euskai.org>
2012-05-01 19:08:34 +0200

    Remove old mootools based menu navbar js

--- includes/html_heads.php
+++ includes/html_heads.php
@@ -14,8 +14,13 @@ $db->select_db("census_".$_SESSION['val_nombre_bbdd']);
 $conf = new cSettings();
 
 // This var is used to access theme style and images.
-$THEMEDIR = 'themes/'.$conf->getSetting('theme');
-if(!is_dir($THEMEDIR)) $THEMEDIR = 'themes/default/'; // Jump to default theme if not exist.
+$theme = $conf->getsetting('theme');
+if($theme!=''){
+	$THEMEDIR = 'themes/'.$theme;
+	if(!is_dir($THEMEDIR)) $THEMEDIR = 'themes/default/'; // Jump to default theme if not exist.
+}else{
+	$THEMEDIR = 'themes/default/'; // Jump to default theme if not exist.
+}
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -28,6 +33,16 @@ if(!is_dir($THEMEDIR)) $THEMEDIR = 'themes/default/'; // Jump to default theme i
 	<link rel="STYLESHEET" href="<?echo $THEMEDIR?>/estilo.css" type="text/css">
 	<link rel="STYLESHEET" href="<?echo $THEMEDIR?>/menuMatic.css" type="text/css">
 	<link rel="STYLESHEET" href="<?echo $THEMEDIR?>/estilo-tabla.css" type="text/css">
+
+	<link type="text/css" href="js/fg-menu/fg.menu.css" media="screen" rel="stylesheet" />
+    	<link type="text/css" href="js/fg-menu/theme/ui.all.css" media="screen" rel="stylesheet" />
+
+	<!-- External jquery js -->
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js" type="text/javascript"></script>
+	
+	<script type="text/javascript" src="js/fg-menu/fg.menu.js"></script>
+
 </head>
 
 <body>
@@ -42,7 +57,7 @@ if(!is_dir($THEMEDIR)) $THEMEDIR = 'themes/default/'; // Jump to default theme i
 				<div class="description">La herramienta eskaut</div>
 			</div>
 		<?}
-
+		// Navigation Menu
 		include 'includes/nav-menu.php';
 		?>
 
--- includes/nav-menu.php
+++ includes/nav-menu.php
@@ -1,45 +1,65 @@
 	<div id="menu">
 		<ul id="nav">
-			<li class="page_item"><a href="#">Censo</a>
-				<ul>
+			<li class="page_item"><a href="#" id="censo">Censo</a>
+				<ul class="hidden">
+					<li><ul>
 					<li><a href="index2.php?section=chavales&task=new">Insertar un nuevo chaval/a</a></li>
 					<li><a href="index2.php?section=chavales">Realizar una búsqueda</a></li>
+					</li></ul>
 				</ul>
 			</li>
-			<!--<li class="page_item"><a href="#">Actividades</a>
-				<ul>
-					<li><a href="">Actividades</a></li>
+			<li class="page_item"><a href="#" id="economia">Economia</a>
+				<ul class="hidden">
+					<li><ul>
+					<li><a href="index2.php?section=economia&subsection=presupuestos">Presupuestos - Balance</a></li>
+					<li><a href="index2.php?section=economia&subsection=librodiario">Libro diario - Ingresos y Gastos</a></li>
+					<li><a href="index2.php?section=economia&subsection=pagoseeb">Pagos EEB</a></li>
+					<li><a href="index2.php?section=economia&subsection=cuotas">Cuotas Familias</a></li>
+					</li></ul>
 				</ul>
-			</li>-->
-			<li class="page_item"><a href="#">Utilidades</a>
-				<ul>
+			</li>
+			<li class="page_item"><a href="#" id="utilidades">Utilidades</a>
+				<ul class="hidden">
+					<li><ul>
 					<li><a href="index2.php?section=exportar">Exportar datos</a></li>
 					<li><a href="index2.php?section=circulares">Circulares</a></li>
+					<li><a href="index2.php?section=notas">Notas</a></li>
+					</li></ul>
 				</ul>
 			</li>
-			<li class="page_item"><a href="#">Configuración</a>
-				<ul>
+			<li class="page_item"><a href="#" id="configuracion">Configuración</a>
+				<ul class="hidden">
+					<li><ul>
 					<li><a href="index2.php?section=grupo">Configuración</a></li>
 					<li><a href="index2.php?section=usuarios">Usuarios</a></li>
 					<li><a href="index2.php?section=ramas">Ramas</a></li>
+					</li></ul>
 				</ul>
 			</li>
 			<?if ($_SESSION['val_rol']=='0'){?>
-			<li class="page_item"><a href="#">Administración</a>
-				<ul>
+			<li class="page_item"><a href="#" id="administracion">Administración</a>
+				<ul class="hidden">
+					<li><ul>
 					<li><a href="index2.php?section=admin_grupos">Grupos</a></li>
 					<li><a href="index2.php?section=admin_privilegios">Privilegios</a></li>
+					</li></ul>
 				</ul>
 			</li>
 			<?}?>
-			<li class="page_item"><a href="index2.php?section=logout">Salir</a></li>
+			<li class="page_item"><a href="index2.php?section=logout" id="salir">Salir</a></li>
 		</ul>
-			
-		<script src="js/mootools-1.2-core-min.js" type="text/javascript" charset="utf-8"></script>
-        	<script src="js/menuMatic_0.67.js" type="text/javascript" charset="utf-8"></script>
-        	<script type="text/javascript" >
-       	        	window.addEvent('domready', function() {
-                      		var myMenu = new MenuMatic();
-               		});
-        	</script>
 	</div>
+		
+
+        <script type="text/javascript" >
+    		
+		$(document).ready(function() {
+			// MENUS    	
+			$("#censo").menu({ content: $("#censo").next().html(),flyOut: true, width:220});
+			$("#economia").menu({ content: $("#economia").next().html(),flyOut: true, width:220});
+			$("#utilidades").menu({ content: $("#utilidades").next().html(),flyOut: true, width:220});
+			$("#configuracion").menu({ content: $("#configuracion").next().html(),flyOut: true, width:220});
+			$("#administracion").menu({ content: $("#administracion").next().html(),flyOut: true, width:220});
+		});
+	
+	</script>
