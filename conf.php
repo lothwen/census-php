39265e6bb52db05ac337d0b67acc68f2b471f821
Daniel Sánchez Ausin <danillo@euskai.org>
2012-05-01 18:23:27 +0200

    Add conf file

---
+++ conf.php
@@ -0,0 +1,15 @@
+<?php
+
+// Database configuration 
+$ddbb_host="localhost";
+$ddbb_user="";
+$ddbb_pass="";
+
+// Other stuff
+$debug=false;
+
+// Version
+$version="v2.0beta";
+
+include "local_conf.php";
+?>
