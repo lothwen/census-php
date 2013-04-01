006f8fa2781c8afde68c0a99be8c33b55cadeaef
Daniel Sánchez <danillo@euskai.org>
2013-04-01 16:58:26 +0200

    Add some changelog message in the index of app

--- portada.php
+++ portada.php
@@ -6,6 +6,16 @@ include 'includes/html_heads.php';
 
 <h4> Bienvenid@ a Census</h4><br />
 
+<h2 style='color:red;font-weight:bold'>ULTIMOS CAMBIOS:</h2>
+<ul>
+	<li><h3 style='color:red'>ARREGLADOS FALLOS EN IMPRESIÓN DE ETIQUETAS</h3></li>
+	<li><h3 style='color:red'>Si faltan códigos postales, habría que completar las fichas de los chavales que les falte el código postal.</h3></li>
+	<li><h3 style='color:red'>Mejorada la ficha del chaval con nuevos campos y mas ordenados</h3></li>
+</ul>
+
+<h3 style='color:green'>Para cualquier pregunta, problema, sugerencia, piropo :-) , podéis contactar conmigo por email en <a href='mailto:danillo@euskai.org'>danillo@euskai.org</a></h3>
+
+<br /><br/>
 <p>Census es una simple aplicación web para llevar el censo de los chavales apuntados a un grupo eskaut. El objetivo
 de la misma es facilitarle la vida al coordinador del grupo, ya que de esta manera, los datos de los chavales se
 encuentran siempre accesibles y centralizados. Con esto evitamos el tener copias de censos desactualizados y permitimos el acceso a los datos de los chavales al resto de monitores.</p>
