ea8a56f7dc67f40665abbdcf5df1f5dea0d2f311
Daniel Sánchez <danillo@euskai.org>
2013-10-07 12:02:24 +0200

    Cleanup and indent HTML widget helper class

--- includes/cHtml.php
+++ includes/cHtml.php
@@ -14,39 +14,6 @@ class cHtml {
 	}
 
 	/**
-	 * Devuelve el codigo html correspondiente a un selector de hora.
-	 * @param string $hourname Nombre del selector de hora.
-	 * @param string $minutesname Nombre del selector de minutos.
-	 * @param integer $initialhour Hora inicial. Default: 08
-	 * @param integer $finalhour Hora Final. Default: 20
-	 * @param integer $minuteinc Incremento a aplicar a los minutos. Default: 1
-	 * return string
-	 */
-	function widget_time($hourname, $minutesname, $minuteinc=15, $initialhour=8, $finalhour=20) {
-
-		//Comprobaciones para que no entren datos incongruentes ;-)
-		if($initialhour<0 || $initialhour>23) $initialhour="00";
-		if($finalhour<0 || $finalhour>23) $finalhour="23";
-
-		$retorno = "<select name='$hourname' class='date'>";
-			
-		for($i=$initialhour;$i<=$finalhour;$i++)
-			$retorno .= "<option value='$i'>".sprintf("%02d",$i)."</option>";
-			
-		$retorno .= "</select>:";
-		
-		$retorno .= "<select name='$minutesname' class='date'>";
-		
-		for($i=0;$i<60;$i+=$minuteinc)
-			$retorno .= "<option value='$i'>".sprintf("%02d",$i)."</option>";
-		
-		$retorno .= "</select></b>";
-
-		return $retorno;
-	
-	}
-
-	/**
 	 * Devuelve el codigo html correspondiente a un selector de fecha.
 	 * @param string $inputname Nombre del selector de fecha.
 	 * @param string $defaultdate Opcional fecha por defecto.
@@ -70,99 +37,84 @@ class cHtml {
 		return $retorno;
 	}	
 
-/**
- * Devuelve el codigo html correspondiente al tag select que se quiere generar.
- * @param string $ddbbtable Nombre de la Tabla de la bbdd de la cual se quiere hacer el select.
- * @param string $ddbbid Campo de la tabla que se va a usar como VALUE en el select.
- * @param array $ddbbname Campos de la tabla que se van a mostrar en el desplegable.
- * @param string $selectname Nombre que se le asignara al select al crear el codigo html.
- * @param string $defaultid VALUE seleccionado por defecto en el select.
- * @param string $onchangescript Nombre y parametros de la funcion javascript a ejecutar en el onchange.
- * @param string $condition condicion de filtrado de la tabla de la bbdd.
- * @return string 
- */
-function widget_select($ddbbtable,$ddbbid,$ddbbname,$selectname,$defaultid=NULL,$onchangescript=NULL,$condition=NULL,$obligatory=NULL) {
+	/**
+	 * Devuelve el codigo html correspondiente al tag select que se quiere generar.
+	 * @param string $ddbbtable Nombre de la Tabla de la bbdd de la cual se quiere hacer el select.
+	 * @param string $ddbbid Campo de la tabla que se va a usar como VALUE en el select.
+	 * @param array $ddbbname Campos de la tabla que se van a mostrar en el desplegable.
+	 * @param string $selectname Nombre que se le asignara al select al crear el codigo html.
+	 * @param string $defaultid VALUE seleccionado por defecto en el select.
+	 * @param string $condition condicion de filtrado de la tabla de la bbdd.
+	 * @return string 
+	 */
+	function widget_select($ddbbtable,$ddbbid,$ddbbname,$selectname,$defaultid=NULL,$condition=NULL) {
 	
-	global $db; 
+		global $db; 
 
-	if (is_array($ddbbname ))  $NAME = implode(",",$ddbbname);
-	else $NAME = $ddbbname;
+		if (is_array($ddbbname ))  $NAME = implode(",",$ddbbname);
+		else $NAME = $ddbbname;
 
-	if ($condition != NULL) $where = " where $condition ";
-	else $where = "";
+		if ($condition != NULL) $where = " where $condition ";
+		else $where = "";
 
-
-	if ($onchangescript!="")
-		$onchange="onChange=\"javascript:$onchangescript\"";
-	else
-		$onchange = "";
-
-	$retorno = "
-			<div id='div$selectname' style='display:inline'>
-			<select class='select' name='$selectname' id='$selectname' $onchange>";
-	if ($obligatory==NULL)
-		$retorno .= "<option value='0'>Seleccione una opcion</option>";
+		$retorno = "<div id='div$selectname' style='display:inline'>
+		    		<select class='select' name='$selectname' id='$selectname'>
+				    <option value='0'>Seleccione una opción</option>";
 	
-	$sql = "select $ddbbid,$NAME from $ddbbtable $where order by $NAME";
-	foreach ($db-> f_sql($sql) as $fila){
-		$cod_cargo = $fila[$ddbbid];
-		if ($cod_cargo==$defaultid) $select = "selected";
-		else $select = "";
-		$NOMBRE = "";
-		if (is_array($ddbbname )) {
-			for ($j=0; $j<count($ddbbname); $j++) {
-				$n = $ddbbname[$j];
-				$NOMBRE .= " ".$fila[$n];
+		$sql = "select $ddbbid,$NAME from $ddbbtable $where order by $NAME";
+		foreach ($db-> f_sql($sql) as $fila){
+			$id = $fila[$ddbbid];
+			if ($id == $defaultid) $select = "selected";
+			if (is_array($ddbbname )) {
+				for ($j=0; $j<count($ddbbname); $j++) {
+					$n = $ddbbname[$j];
+					$NOMBRE .= " ".$fila[$n];
+				}
 			}
+			else $NOMBRE = $fila[$ddbbname];
+			$retorno .= "<option value='$id' $select>".$NOMBRE."</option>";	
 		}
-		else $NOMBRE = $fila[$ddbbname];
 
-		$retorno .= "<option value='$cod_cargo' $select>".$NOMBRE."</option>";	
+		$retorno .= "</select></div>";
+		return $retorno;
 	}
 
-	$retorno .= "</select></div>";
-	return $retorno;
-}
 
-
-function widget_table($width,$header_list,$filas,$tamanhos=NULL, $align=NULL) {
+	function widget_table($width,$header_list,$filas,$tamanhos=NULL, $align=NULL) {
 	
-	echo "<table class='table_list' width='$width' align='center' >";
+		echo "<table class='table_list' width='$width' align='center' >";
  
-     if (count($filas)>0) {
-         echo "
-             <thead>
-                 <tr>";
-         for ($i=0; $i<count($header_list); $i++)
-             echo "      <th width='".$tamanhos[$i]."'>".$header_list[$i]."</th> \n";
+     		if (count($filas)>0) {
+         		echo "
+             		<thead>
+                 	  <tr>";
+         		for ($i=0; $i<count($header_list); $i++)
+             			echo "    <th width='".$tamanhos[$i]."'>".$header_list[$i]."</th> \n";
+             			echo "
+                 			  </tr>
+             				</thead>";
+             	}
  
-             echo "
-                 </tr>
-             </thead>
-             ";
-             }
+     		echo "<tbody>";
+    		if (count($filas)<1) {
+         		echo "<tr class='linea2' colspan='".count($header_list)."'><td>No se han encontrado resultados.</td></tr>";
+         	}
  
-     echo "<tbody>";
-     if (count($filas)<1) {
-         echo "<tr class='linea2' colspan='".count($header_list)."'><td>No se han encontrado resultados.</td></tr>";
-         }
- 
-     for ($i=0; $i<count($filas); $i++) {
-         $fila = $filas[$i];
-         $class = "linea2";
-         if ($i%2==0)
-             $class = "linea1";
-         echo "<tr class='$class'>";
-         for ($j=0; $j<count($fila); $j++)
-             echo "<td align='".$align[$j]."'>".$fila[$j]."</td>";
-         echo "</tr>";
-         }
-     echo " 
-         </tbody>
-         </table>
-         ";
- 
-}
+     		for ($i=0; $i<count($filas); $i++) {
+         		$fila = $filas[$i];
+         		$class = "linea2";
+         		if ($i%2==0)
+             			$class = "linea1";
+         		echo "<tr class='$class'>";
+         		for ($j=0; $j<count($fila); $j++)
+             			echo "<td align='".$align[$j]."'>".$fila[$j]."</td>";
+         		echo "</tr>";
+         	}
+
+     		echo "</tbody>
+         	   </table>";
+
+	}
 
 }
 ?>
